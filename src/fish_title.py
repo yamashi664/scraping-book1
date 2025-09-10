c1558df431368d919e9223c20b9f9adebc3b0649
yamashi <flw.fe.ch@gmail.com>
2025-09-10 17:50:41 +0900

    modified fish_title.py

--- src/fish_title.py
+++ src/fish_title.py
@@ -5,5 +5,13 @@ with open('fish.html', encoding = 'utf-8') as fp:
 
 soup = BeautifulSoup(html_str, 'html5lib')
 
-title = soup.find('title')
-print(title)
+for h2 in soup.find_all('h2'):
+    if h2.string == 'ウナギ':
+        for e in h2.next_siblings:
+            if e.name == 'p':
+                if e['class'][0] == 'price':
+                    print(e.string)
+
+yyy = soup.find('body')       
+print("string:", yyy.string)
+print("text:", yyy.text)
